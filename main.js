80657c0d40e3ed7a6cef2d93b1a090059530031b
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-02-26 18:10:53 +0300

    Add hash method

--- main.js
+++ main.js
@@ -3,4 +3,18 @@ class HashMap {
     this.capacity = 16;
     this.loadfactor = 0.75;
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % this.capacity;
+    }
+
+    return hashCode;
+  }
 }
+
+const hashMap = new HashMap();
