d466c653e1fdf8188f57a83260bf2f91e1675924
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-03-02 08:55:46 +0300

    Add set, get, has, remove methods and implement with a LinkedList to prevent collisions

--- main.js
+++ main.js
@@ -1,7 +1,112 @@
+class Node {
+  constructor(key = null, value = null, next = null) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  append(key, value) {
+    if (!this.head) {
+      this.head = new Node(key, value);
+      this.length++;
+      return;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (currentNode.key === key) {
+        currentNode.value = value;
+        return;
+      }
+
+      if (!currentNode.nextNode) {
+        currentNode.nextNode = new Node(key, value);
+        this.length++;
+        return;
+      }
+
+      currentNode = currentNode.nextNode;
+    }
+  }
+
+  get(key) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    do {
+      if (currentNode.key == key) {
+        return currentNode.value;
+      } else {
+        currentNode = currentNode.nextNode;
+      }
+    } while (currentNode != null);
+
+    return null;
+  }
+
+  has(key) {
+    if (!this.head) {
+      return false;
+    }
+
+    let currentNode = this.head;
+
+    do {
+      if (currentNode.key == key) {
+        return true;
+      } else {
+        currentNode = currentNode.nextNode;
+      }
+    } while (currentNode != null);
+
+    return false;
+  }
+
+  remove(key) {
+    if (!this.head) {
+      return false;
+    }
+
+    if (this.head.key == key) {
+      this.head = null;
+      this.length -= 1;
+      return true;
+    }
+
+    let previousNode = this.head;
+    let currentNode = previousNode.nextNode;
+
+    do {
+      if (currentNode.key == key) {
+        previousNode.nextNode = currentNode.nextNode;
+        this.length -= 1;
+        return true;
+      } else {
+        previousNode = currentNode;
+        currentNode = currentNode.nextNode;
+      }
+    } while (currentNode != null);
+
+    return false;
+  }
+}
+
 class HashMap {
   constructor(capacity = 16, loadfactor = 0.75) {
-    this.capacity = 16;
-    this.loadfactor = 0.75;
+    this.capacity = capacity;
+    this.loadfactor = loadfactor;
+    this.buckets = [];
   }
 
   hash(key) {
@@ -15,6 +120,56 @@ class HashMap {
 
     return hashCode;
   }
+
+  boundscheck(index) {
+    if (index < 0 || index >= this.capacity) {
+      throw new Error("Trying to access index out of bounds");
+    }
+  }
+
+  set(key, value) {
+    let hashValue = this.hash(key);
+    this.boundscheck(hashValue);
+
+    if (this.buckets[hashValue] == undefined) {
+      this.buckets[hashValue] = new LinkedList();
+    }
+
+    this.buckets[hashValue].append(key, value);
+  }
+
+  get(key) {
+    let hashValue = this.hash(key);
+    this.boundscheck(hashValue);
+
+    if (this.buckets[hashValue] == undefined) {
+      return null;
+    } else {
+      return this.buckets[hashValue].get(key);
+    }
+  }
+
+  has(key) {
+    let hashValue = this.hash(key);
+    this.boundscheck(hashValue);
+
+    if (this.buckets[hashValue] == undefined) {
+      return false;
+    } else {
+      return this.buckets[hashValue].has(key);
+    }
+  }
+
+  remove(key) {
+    let hashValue = this.hash(key);
+    this.boundscheck(hashValue);
+
+    if (this.buckets[hashValue] == undefined) {
+      return false;
+    } else {
+      return this.buckets[hashValue].remove(key);
+    }
+  }
 }
 
-const hashMap = new HashMap();
+const hashmap = new HashMap();
